214a6c9630f5edfd6c4404bbfe15e413d1133c01
Tanish Raorane <140275713+Tanish-Raorane@users.noreply.github.com>
2025-08-19 20:40:38 +0100

    isColliding Cane Check

--- Assets/Scenes/asylum materials/scripts/CaneImpactSound.cs
+++ Assets/Scenes/asylum materials/scripts/CaneImpactSound.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class CaneImpactSound : MonoBehaviour
 {
+
+    public GameObject senmagWorkspace;
+    private GameObject defaultCursor;
+
+    private bool firstTime = false;
+
+
+    
+
+
     [Header("Wwise")]
     // Drag your cane impact Play Event from the Wwise Picker here.
     public AK.Wwise.Event ImpactEvent;
@@ -17,16 +27,46 @@ public class CaneImpactSound : MonoBehaviour
 
     // This is a built-in Unity function that is automatically called
     // when this object's collider hits another object's collider.
-    private void OnCollisionEnter(Collision collision)
-    {
-        // Check if enough time has passed since the last sound was played.
-        if (Time.time >= lastPlayTime + CooldownSeconds)
-        {
-            // If the cooldown has passed, update the last play time to now.
-            lastPlayTime = Time.time;
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    // Check if enough time has passed since the last sound was played.
+    //    if (Time.time >= lastPlayTime + CooldownSeconds)
+    //    {
+    //        // If the cooldown has passed, update the last play time to now.
+    //        lastPlayTime = Time.time;
 
-            // Post the Wwise Event to play the sound from this cane's location.
-            ImpactEvent.Post(gameObject);
+    //        // Post the Wwise Event to play the sound from this cane's location.
+    //        ImpactEvent.Post(gameObject);
+    //    }
+    //}
+
+    private void Update()
+    {
+        if (senmagWorkspace.transform.GetChild(0).GetChild(1) == null || senmagWorkspace.transform.childCount == 0)
+        {
+            return;
         }
+
+        if (!firstTime)
+        {
+            defaultCursor = senmagWorkspace.transform.GetChild(0).GetChild(1).gameObject;
+
+            firstTime = true;
+        }
+
+
+        if (defaultCursor.GetComponent<Senmag_stylusControl>().isColliding)
+        {
+            // Check if enough time has passed since the last sound was played.
+            if (Time.time >= lastPlayTime + CooldownSeconds)
+            {
+                // If the cooldown has passed, update the last play time to now.
+                lastPlayTime = Time.time;
+
+                // Post the Wwise Event to play the sound from this cane's location.
+                ImpactEvent.Post(gameObject);
+            }
+        }
+
     }
 }
