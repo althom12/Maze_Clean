0371f5644f5bb95fd6ea8e80e9de47eb8b3fb403
Alex Thompson <alexanderkthompson@gmail.com>
2025-08-19 20:20:20 +0100

    Cane script

---
+++ Assets/Scenes/asylum materials/scripts/CaneImpactSound.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaneImpactSound : MonoBehaviour
+{
+    [Header("Wwise")]
+    // Drag your cane impact Play Event from the Wwise Picker here.
+    public AK.Wwise.Event ImpactEvent;
+
+    [Header("Settings")]
+    // A cooldown in seconds to prevent the sound from spamming.
+    public float CooldownSeconds = 0.2f;
+
+    // A private variable to track when the sound was last played.
+    private float lastPlayTime = -1f;
+
+    // This is a built-in Unity function that is automatically called
+    // when this object's collider hits another object's collider.
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if enough time has passed since the last sound was played.
+        if (Time.time >= lastPlayTime + CooldownSeconds)
+        {
+            // If the cooldown has passed, update the last play time to now.
+            lastPlayTime = Time.time;
+
+            // Post the Wwise Event to play the sound from this cane's location.
+            ImpactEvent.Post(gameObject);
+        }
+    }
+}
