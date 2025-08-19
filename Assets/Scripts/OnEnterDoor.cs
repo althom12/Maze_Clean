464097e0c61f51ccfbd5f0a9f723b1b1f322ccf1
Tanish Raorane <tanishraorane4@gmail.com>
2025-08-19 23:33:30 +0100

    Doors done

--- Assets/Scripts/OnEnterDoor.cs
+++ Assets/Scripts/OnEnterDoor.cs
@@ -1,27 +1,38 @@
 using SenmagHaptic;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OnEnterDoor : MonoBehaviour
 {
-    
+    private bool shouldOpen = false;
+    private Transform door;
+    public float openAngle = -75f;  // target Y angle
+    public float rotationSpeed = 50f; // degrees per second
+
     void Start()
     {
-        
+        door = transform.GetChild(0).GetChild(0);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.GetComponentInParent<Senmag_HapticCursor>() || other.gameObject.tag == "Door")
+        if (other.gameObject.GetComponentInParent<Senmag_HapticCursor>() || other.CompareTag("Door"))
         {
-            gameObject.transform.GetChild(0).GetChild(0).localEulerAngles = new Vector3(0, -75f, 0);
+            shouldOpen = true;
         }
-
     }
 
     void Update()
     {
-        
+        if (shouldOpen)
+        {
+            // Current rotation
+            Quaternion currentRotation = gameObject.transform.localRotation;
+
+            // Target rotation (-75f on Y)
+            Quaternion targetRotation = Quaternion.Euler(0, openAngle, 0);
+
+            // Rotate smoothly towards target
+            gameObject.transform.localRotation = Quaternion.RotateTowards(currentRotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
